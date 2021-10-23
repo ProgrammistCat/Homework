33f87639cc18a39a74638080e4eded4ef1b587e7
ProgrammistCat <sofiapogorelova94@gmail.com>
2021-10-23 02:52:57 +0200

    Initial commit

---
+++ Homework/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.VisualBasic;
+
+namespace Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задача №1:
+            
+            Console.Write("1-ое число (дробное): ");
+            string text = Console.ReadLine();
+            double first = Convert.ToDouble(text);
+            
+            Console.Write("2-ое число (целое): ");
+            text = Console.ReadLine();
+            int second = Int32.Parse(text);
+            
+            double result = Power(first, second);
+            Console.WriteLine(result);
+        }
+
+        static double Power(double first, int second)
+        {
+            double firstnumber = first;
+
+            if (second > 0)
+            {
+                for (int i = 0; i < second - 1; i++)
+                {
+                    first *= firstnumber;
+                }
+            }
+            else if (second == 0)
+            {
+                first = 1;
+            }
+            else
+            {
+                second = second * -1;
+                    
+                for (int i = 0; i < second - 1; i++)
+                {
+                    first *= firstnumber;
+                }
+
+                first = 1 / first;
+            }
+
+            
+            double result = first;
+            return result;
+        }
+    }
+}
